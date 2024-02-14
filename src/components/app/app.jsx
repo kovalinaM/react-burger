a2b3a9d6d7121e86324250db409fdf6de8b3b155
kovalinaM <sunshining1106@mail.ru>
2024-02-14 10:08:14 +0300

    Подготовка структуры проекта

---
+++ src/components/app/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppHeader from '../app-header/app-header'
+import stylesApp from './app.module.css';
+
+export default function App() {
+    return (
+        <AppHeader/>
+    )
+} 
