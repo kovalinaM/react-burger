a1b60cee9603cc76c9bb54bd14b5eeed5103558f
kovalinaM <sunshining1106@mail.ru>
2024-02-14 14:44:29 +0300

    Верстка app-header

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import AppHeader from '../app-header/app-header'
-import stylesApp from './app.module.css';
+import React from "react";
+import AppHeader from "../app-header/app-header";
+import BurgerIngredients from '../burger-ingredients/burger-ingredients'
+import BurgerConstructor from '../burger-constructor/burger-constructor'
+
+import stylesApp from "./app.module.css";
 
 export default function App() {
-    return (
-        <AppHeader/>
-    )
-} 
+  return (
+    <React.Fragment>
+      <div className={stylesApp.container}>
+        <AppHeader />
+        <main>
+            тело
+        </main>
+      </div>
+    </React.Fragment>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import './index.css'
 import App from "./components/app/app";
 
-const root = ReactDOM.createRoot(document.querySelector("#root"));
+const elRoot:HTMLElement = document.querySelector("#root") as HTMLElement;
+const root = ReactDOM.createRoot(elRoot);
 root.render(
   <React.StrictMode>
     <App />
