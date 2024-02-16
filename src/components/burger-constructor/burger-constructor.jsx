7a60cf1c6f539309dbbfe5322ff06d42fd538ccb
Kovaleva Marina <kirienko1995@mail.ru>
2024-02-16 17:43:55 +0300

    часть верстки компонента BurgerConstructor
    Пушу только потому что ближайшие пару дней дорабатывать буду за другим компом. А так этот компонент еще делать и делать.

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import {
   ConstructorElement,
-  CurrencuIcon,
+  CurrencyIcon,
   Button,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
+import styles from "./burger-constructor.module.css";
+
 export default function BurgerConstructor(props) {
   return (
-    <section>
-      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-        {/* <ConstructorElement
+    <section className={`${styles.constructor} mt-25`}>
+      {/* <div>
+        <div>
+          <ConstructorElement
+            text={bun.name + " (Верх)"}
+            isLocked={true}
+            price={bun.price}
+            thumbnail={bun.image}
+            type="top"
+          />
+        </div>
+        <div className={style.list}>
+          <ConstructorElement
           type="top"
           isLocked={true}
           text="Краторная булка N-200i (верх)"
@@ -27,7 +39,26 @@ export default function BurgerConstructor(props) {
           text="Краторная булка N-200i (низ)"
           price={200}
           thumbnail={img}
-        /> */}
+        />
+        </div>
+        <div className="ml-15">
+          <ConstructorElement
+            text={bun.name + " (Низ)"}
+            isLocked={true}
+            price={bun.price}
+            thumbnail={bun.image}
+            type="bottom"
+          />
+        </div>
+      </div> */}
+      <div className={`${styles.total} mt-10`}>
+        <div className={styles.value}>
+          <span></span>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button htmlType="button" type="primary" size="large">
+          Оформить заказ
+        </Button>
       </div>
     </section>
   );
