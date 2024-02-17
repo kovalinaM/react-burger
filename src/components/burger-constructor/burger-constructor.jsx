18e404c42ec0bf63315118813a64b389c173b882
Kovaleva Marina <sunshining1106@mail.ru>
2024-02-17 15:17:45 +0300

    feat: верстка компонента конструктора бургера

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,59 +1,71 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {
   ConstructorElement,
   CurrencyIcon,
+  DragIcon,
   Button,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import styles from "./burger-constructor.module.css";
 
 export default function BurgerConstructor(props) {
+  const handleIngredients = props.ingredients;
+  const bun = handleIngredients[0];
+  const fillings = handleIngredients.slice(1, handleIngredients.length);
+  console.log('1',fillings)
+  const orderSum = (bun, fillings) => {
+    let sum = bun ? bun.price : 0;
+    fillings &&
+      fillings.map((item) => {
+        return (sum += item.price);
+      });
+    return sum;
+  };
+
   return (
-    <section className={`${styles.constructor} mt-25`}>
-      {/* <div>
-        <div>
+    <section className={`${styles.constructor} mt-25 mb-10`}>
+      <div className={styles.ingredients_container}>
+        <div className="ml-8">
           <ConstructorElement
-            text={bun.name + " (Верх)"}
+            text={`${bun.name}` + " (Верх)"}
             isLocked={true}
             price={bun.price}
-            thumbnail={bun.image}
+            thumbnail={bun.image_mobile}
             type="top"
           />
         </div>
-        <div className={style.list}>
-          <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          thumbnail={img}
-        />
-        <ConstructorElement
-          text="Краторная булка N-200i (верх)"
-          price={50}
-          thumbnail={img}
-        />
-        <ConstructorElement
-          type="bottom"
-          isLocked={true}
-          text="Краторная булка N-200i (низ)"
-          price={200}
-          thumbnail={img}
-        />
+        <div>
+          <ul className={`${styles.list} custom-scroll`}>
+          {fillings.flatMap((item) => {
+            return Array.from({ length: item.__v }, (_, index) => (
+              <li className={styles.item} key={`${item._id}-${index}`}>
+                <DragIcon type="primary" />
+                <ConstructorElement
+                  text={item.name}
+                  price={item.price}
+                  thumbnail={item.image_mobile}
+                />
+              </li>
+            ));
+          })}
+          </ul>
         </div>
-        <div className="ml-15">
+        <div className="ml-8">
           <ConstructorElement
-            text={bun.name + " (Низ)"}
+            text={`${bun.name}` + " (Низ)"}
             isLocked={true}
             price={bun.price}
-            thumbnail={bun.image}
+            thumbnail={bun.image_mobile}
             type="bottom"
           />
         </div>
-      </div> */}
+      </div>
       <div className={`${styles.total} mt-10`}>
         <div className={styles.value}>
-          <span></span>
+          <span className="text text_type_main-large">
+            {orderSum(bun, fillings)}
+          </span>
           <CurrencyIcon type="primary" />
         </div>
         <Button htmlType="button" type="primary" size="large">
@@ -63,3 +75,22 @@ export default function BurgerConstructor(props) {
     </section>
   );
 }
+
+BurgerConstructor.propTypes = {
+  ingredients: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.oneOf(["bun", "main", "sauce"]).isRequired,
+      proteins: PropTypes.number,
+      fat: PropTypes.number,
+      carbohydrates: PropTypes.number,
+      calories: PropTypes.number,
+      price: PropTypes.number,
+      image: PropTypes.string,
+      image_mobile: PropTypes.string.isRequired,
+      image_large: PropTypes.string,
+      __v: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
--- src/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -25,17 +25,9 @@ export default function BurgerIngredient(props) {
 
 BurgerIngredient.propTypes = {
   ingredient: PropTypes.shape({
-    _id: PropTypes.string,
     name: PropTypes.string.isRequired,
-    type: PropTypes.oneOf(["bun", "main", "sauce"]).isRequired,
-    proteins: PropTypes.number,
-    fat: PropTypes.number,
-    carbohydrates: PropTypes.number,
-    calories: PropTypes.number,
-    price: PropTypes.number,
+    price: PropTypes.number.isRequired,
     image: PropTypes.string.isRequired,
-    image_mobile: PropTypes.string,
-    image_large: PropTypes.string,
-    __v: PropTypes.number,
+    __v: PropTypes.number.isRequired,
   })
 }
