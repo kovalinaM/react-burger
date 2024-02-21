82ffd97570411ab1e4be8bf2780a1ea9dade3c51
Kovaleva Marina <kirienko1995@mail.ru>
2024-02-21 16:17:46 +0300

    fest: доработать вывод данных от API в IngredientDetails
    Заменить вывод захардкожанных данных на данные, полученные от API  с описанием конкретного ингредиента

--- src/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -1,33 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import {
   Counter,
   CurrencyIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
+import Modal from "../modal/modal";
+import IngredientDetails from "../ingredient-details/ingredient-details";
+
 import styles from "./burger-ingredient.module.css";
 export default function BurgerIngredient(props) {
   const ingredient = props.ingredient;
+  const [modalIsActive, setModalActive] = useState(false);
+
+  const onClose = () => {
+    setModalActive(false);
+};
+
   return (
-    <article className={styles.card}>
-      {(ingredient.__v > 0) && <Counter count={ingredient.__v} size="default" extraClass="m-1" /> } 
-      <div className="mb-1">
-        <img src={ingredient.image} alt={ingredient.name} />
-      </div>
-      <div className={styles.price} >
-        <span className="text text_type_main-medium">{ingredient.price}</span>
-        <CurrencyIcon type="primary" />
-      </div>
-      <div className={`${styles.name} text text_type_main-default mt-1 pb-3`}>{ingredient.name}</div>
-    </article>
+    <>
+      <article className={styles.card} onClick={()=>setModalActive(true)}>
+        {ingredient.__v > 0 && (
+          <Counter count={ingredient.__v} size="default" extraClass="m-1" />
+        )}
+        <div className="mb-1">
+          <img src={ingredient.image} alt={ingredient.name} />
+        </div>
+        <div className={styles.price}>
+          <span className="text text_type_main-medium">{ingredient.price}</span>
+          <CurrencyIcon type="primary" />
+        </div>
+        <div className={`${styles.name} text text_type_main-default mt-1 pb-3`}>
+          {ingredient.name}
+        </div>
+      </article>
+      {modalIsActive && (
+        <Modal header="Детали ингредиента" onClose={onClose}>
+          <IngredientDetails {...ingredient}/>
+        </Modal>
+      )}
+    </>
   );
 }
 
 BurgerIngredient.propTypes = {
   ingredient: PropTypes.shape({
+    _id: PropTypes.string,
     name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
+    type: PropTypes.oneOf(["bun", "main", "sauce"]).isRequired,
+    proteins: PropTypes.number,
+    fat: PropTypes.number,
+    carbohydrates: PropTypes.number,
+    calories: PropTypes.number,
+    price: PropTypes.number,
     image: PropTypes.string.isRequired,
-    __v: PropTypes.number.isRequired,
-  })
-}
+    image_mobile: PropTypes.string,
+    image_large: PropTypes.string,
+    __v: PropTypes.number,
+  }),
+};
--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,36 +1,67 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./ingredient-details.module.css";
 
-const IngredientDetails = ({ingredient}) => {
-  console.log(ingredient)
-  return (
-    <div className={styles.product_card}>
-      <img
-        src="https://code.s3.yandex.net/react/code/sp_1-large.png"
-        alt="Биокотлета из марсианской Магнолии"
-        className="mb-4"
-      />
-      <h2 className="mb-8">{ingredient?.name}</h2>
-      <div className={`${styles.product_details} mb-15`}>
-        <div className={`${styles.product_detail} text text_type_main-small text_color_inactive`}>
-          <p className="mb-2">Калории,ккал</p>
-          <p>244,4</p>
-        </div>
-        <div className={`${styles.product_detail} text text_type_main-small text_color_inactive`}>
-          <p className="mb-2">Белки, г</p>
-          <p>12,2</p>
-        </div>
-        <div className={`${styles.product_detail} text text_type_main-small text_color_inactive`}>
-          <p className="mb-2">Жиры, г</p>
-          <p>17,2</p>
-        </div>
-        <div className={`${styles.product_detail} text text_type_main-small text_color_inactive`}>
-          <p className="mb-2">Углеводы, г</p>
-          <p>10,2</p>
+const IngredientDetails = (ingredient) => {
+  if (!ingredient) {
+    return "Загружаю сведения об ингредиенте...";
+  } else {
+    return (
+      <div className={styles.product_card}>
+        <img
+          src={ingredient?.image_large}
+          alt={ingredient?.name}
+          width="480"
+          height="240"
+          className="mb-4"
+        />
+        <h2 className="mb-8">{ingredient?.name}</h2>
+        <div className={`${styles.product_details} mb-15`}>
+          <div
+            className={`${styles.product_detail} text text_type_main-small text_color_inactive`}
+          >
+            <p className="mb-2">Калории,ккал</p>
+            <p>{ingredient?.calories}</p>
+          </div>
+          <div
+            className={`${styles.product_detail} text text_type_main-small text_color_inactive`}
+          >
+            <p className="mb-2">Белки, г</p>
+            <p>{ingredient?.proteins}</p>
+          </div>
+          <div
+            className={`${styles.product_detail} text text_type_main-small text_color_inactive`}
+          >
+            <p className="mb-2">Жиры, г</p>
+            <p>{ingredient?.fat}</p>
+          </div>
+          <div
+            className={`${styles.product_detail} text text_type_main-small text_color_inactive`}
+          >
+            <p className="mb-2">Углеводы, г</p>
+            <p>{ingredient?.carbohydrates}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default IngredientDetails;
+
+IngredientDetails.propTypes = {
+  ingredient: PropTypes.shape({
+    _id: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(["bun", "main", "sauce"]).isRequired,
+    proteins: PropTypes.number,
+    fat: PropTypes.number,
+    carbohydrates: PropTypes.number,
+    calories: PropTypes.number,
+    price: PropTypes.number,
+    image: PropTypes.string.isRequired,
+    image_mobile: PropTypes.string,
+    image_large: PropTypes.string,
+    __v: PropTypes.number,
+  }),
+};
