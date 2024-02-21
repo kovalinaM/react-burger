81355d41e67f9d6da57a0b77c7c3bf4e0bb156f3
Kovaleva Marina <kirienko1995@mail.ru>
2024-02-21 16:51:32 +0300

    feat: добавить propTypes для ModalOverlay

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import styles from "./modal-overlay.module.css"
+import PropTypes from "prop-types";
 
+import styles from "./modal-overlay.module.css";
 
 const ModalOverlay = (props) => {
-    return <div className={styles.modal_overlay} onClick={props.onClick}/>;
-}
+  return <div className={styles.modal_overlay} onClick={props.onClick} />;
+};
 
 export default ModalOverlay;
+
+ModalOverlay.propTypes = {
+  onClick: PropTypes.func,
+};
