655614f0ec064ab37594392c2c0cd81de596a85a
Kovaleva Marina <kirienko1995@mail.ru>
2024-02-22 08:48:35 +0300

    fix: исправить значение атрибута key
    Для атрибута key  использовать уникальный id для ингредиента _id

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,10 +1,10 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import BurgerIngredient from "../burger-ingredient/burger-ingredient";
 import styles from "./burger-ingredients.module.css";
 
-export default function BurgerIngredients(props)  {
+export default function BurgerIngredients(props) {
   const [current, setCurrent] = useState("buns");
   const ingredients = props.ingredients;
 
@@ -26,8 +26,8 @@ export default function BurgerIngredients(props)  {
     const mainElement = document.getElementById("main");
 
     const bunPosition = bunElement.getBoundingClientRect().top;
-    const saucePosition  = sauceElement.getBoundingClientRect().top;
-    const mainPosition  = mainElement.getBoundingClientRect().top;
+    const saucePosition = sauceElement.getBoundingClientRect().top;
+    const mainPosition = mainElement.getBoundingClientRect().top;
 
     if (scrollPosition >= mainPosition) {
       setCurrent("main");
@@ -42,25 +42,29 @@ export default function BurgerIngredients(props)  {
     <section className="pt-10 pb-10">
       <h1 className="text text_type_main-large">Соберите бургер</h1>
       <div style={{ display: "flex" }} className="mt-5">
-        <Tab 
-        value="buns" 
-        active={current === "buns"} 
-        onClick={handleTabClick}>
+        <Tab value="buns" active={current === "buns"} onClick={handleTabClick}>
           Булки
         </Tab>
-        <Tab value="sauces" active={current === "sauces"} onClick={handleTabClick}>
+        <Tab
+          value="sauces"
+          active={current === "sauces"}
+          onClick={handleTabClick}
+        >
           Соусы
         </Tab>
         <Tab value="main" active={current === "main"} onClick={handleTabClick}>
           Начинки
         </Tab>
       </div>
-      <div onScroll={handleScroll} className={`${styles.scrollarea} custom-scroll`}>
+      <div
+        onScroll={handleScroll}
+        className={`${styles.scrollarea} custom-scroll`}
+      >
         <div id="buns" className="mt-10">
           <h2 className="text text_type_main-medium mb-6">Булки</h2>
           <ul className={`${styles.list} mt-6`}>
-            {listBun.map((ingredient, index) => (
-              <li key={index}>
+            {listBun.map((ingredient) => (
+              <li key={ingredient._id}>
                 <BurgerIngredient ingredient={ingredient} />
               </li>
             ))}
@@ -69,8 +73,8 @@ export default function BurgerIngredients(props)  {
         <div id="sauces" className="mt-10">
           <h2 className="text text_type_main-medium  mb-6">Соусы</h2>
           <ul className={styles.list}>
-            {listSauces.map((ingredient, index) => (
-              <li key={index}>
+            {listSauces.map((ingredient) => (
+              <li key={ingredient._id}>
                 <BurgerIngredient ingredient={ingredient} />
               </li>
             ))}
@@ -79,8 +83,8 @@ export default function BurgerIngredients(props)  {
         <div id="main" className="mt-10">
           <h2 className="text text_type_main-medium  mb-6">Начинки</h2>
           <ul className={styles.list}>
-            {listFillings.map((ingredient, index) => (
-              <li key={index}>
+            {listFillings.map((ingredient) => (
+              <li key={ingredient._id}>
                 <BurgerIngredient ingredient={ingredient} />
               </li>
             ))}
@@ -89,7 +93,7 @@ export default function BurgerIngredients(props)  {
       </div>
     </section>
   );
-};
+}
 
 BurgerIngredients.propTypes = {
   ingredients: PropTypes.arrayOf(
