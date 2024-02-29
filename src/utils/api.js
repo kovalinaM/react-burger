0da4362f3586e1f806ba41521d42d9888e1130cc
Kovaleva Marina <kirienko1995@mail.ru>
2024-02-29 12:53:04 +0300

    fix: вынести константу с url в отдельный файл

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://norma.nomoreparties.space/api";
+import {BASE_URL} from "./constants"
 
 export const checkResponse = (res) => {
     return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
