62c6badc58bbbf53241d92e91761dc7623aac1f9
Kovaleva Marina <kirienko1995@mail.ru>
2024-03-01 17:34:35 +0300

    переписать логику модального окна ингредиента (redux)
    добавить экшены, редьюсер

--- src/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -1,41 +1,60 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {
   Counter,
   CurrencyIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
-
+import { useSelector, useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 import Modal from "../modal/modal";
 import IngredientDetails from "../ingredient-details/ingredient-details";
-
-import {ingredientType} from '../../utils/types'
-
+import { ingredientType } from "../../utils/types";
 import styles from "./burger-ingredient.module.css";
-const BurgerIngredient = (props) => {
-  const ingredient = props.ingredient;
-  const [modalIsActive, setModalActive] = useState(false);
+import {
+  OPEN_INGREDIENT_MODAL,
+  CLOSE_INGREDIENT_MODAL,
+} from "../../services/actions/ingredient-details";
+
+const BurgerIngredient = ({ ingredient, count }) => {
+  const { modalIsActive } = useSelector((store) => store.ingredientDetails);
+  const dispatch = useDispatch();
 
   const onClose = () => {
-    setModalActive(false);
+    dispatch({
+      type: CLOSE_INGREDIENT_MODAL,
+    });
+  };
+
+  const onOpenModal = () => {
+    dispatch({
+      type: OPEN_INGREDIENT_MODAL,
+    });
   };
 
   return (
     <>
-      <article className={styles.card} onClick={() => setModalActive(true)}>
-        {ingredient.__v > 0 && (
-          <Counter count={ingredient.__v} size="default" extraClass="m-1" />
-        )}
-        <div className="mb-1">
-          <img src={ingredient.image} alt={ingredient.name} />
-        </div>
-        <div className={styles.price}>
-          <span className="text text_type_main-medium">{ingredient.price}</span>
-          <CurrencyIcon type="primary" />
-        </div>
-        <div className={`${styles.name} text text_type_main-default mt-1 pb-3`}>
-          {ingredient.name}
-        </div>
-      </article>
+      <li>
+        <article className={styles.card} onClick={onOpenModal}>
+          {count > 0 && (
+            <Counter count={count} size="default" extraClass="m-1" />
+          )}
+          <div className="mb-1">
+            <img src={ingredient.image} alt={ingredient.name} />
+          </div>
+          <div className={styles.price}>
+            <span className="text text_type_main-medium">
+              {ingredient.price}
+            </span>
+            <CurrencyIcon type="primary" />
+          </div>
+          <div
+            className={`${styles.name} text text_type_main-default mt-1 pb-3`}
+          >
+            {ingredient.name}
+          </div>
+        </article>
+      </li>
+
       {modalIsActive && (
         <Modal header="Детали ингредиента" onClose={onClose}>
           <IngredientDetails ingredient={ingredient} />
@@ -43,10 +62,11 @@ const BurgerIngredient = (props) => {
       )}
     </>
   );
-}
+};
 
 BurgerIngredient.propTypes = {
   ingredient: ingredientType.isRequired,
+  count: Number,
 };
 
 export default React.memo(BurgerIngredient);
--- src/components/ingredients-category/ingredients-category.jsx
+++ src/components/ingredients-category/ingredients-category.jsx
@@ -5,24 +5,26 @@ import styles from "./ingredients-category.module.css";
 import BurgerIngredient from "../burger-ingredient/burger-ingredient";
 
 const IngredientsCategory = ({ title, type, ingredients }) => {
-    return (
-        <div id={`${type}`} className="mt-10">
-            <h2 className="text text_type_main-medium mb-6">{title}</h2>
-            <ul className={`${styles.list} mt-6`}>
-                {ingredients.map((ingredient) => (
-                    <li key={ingredient._id}>
-                        <BurgerIngredient ingredient={ingredient} />
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div id={`${type}`} className="mt-10">
+      <h2 className="text text_type_main-medium mb-6">{title}</h2>
+      <ul className={`${styles.list} mt-6`}>
+        {ingredients.map((ingredient) => (
+          <BurgerIngredient
+            key={ingredient._id}
+            ingredient={ingredient}
+            count={0}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 IngredientsCategory.propTypes = {
-    title: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    ingredients: PropTypes.arrayOf(ingredientType).isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  ingredients: PropTypes.arrayOf(ingredientType).isRequired,
 };
 
 export default IngredientsCategory;
