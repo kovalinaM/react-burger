b76f34406d45e87e27ae9af3fcee490566f53b66
Kovaleva Marina <sunshining1106@mail.ru>
2024-03-19 17:08:00 +0300

    feat: добавить функцию обновления токена и функции остальных запросов по работе с пользователем

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,119 @@
-import {BASE_URL} from "./constants"
+import { BASE_URL } from "./constants";
 
 export const checkResponse = (res) => {
-    return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
+  return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 };
 
 export function getIngredients() {
-    return fetch(`${BASE_URL}/ingredients`)
-        .then(checkResponse)
+  return fetch(`${BASE_URL}/ingredients`).then(checkResponse);
+}
+
+export const postOrder = ({ ingredients }) => {
+  return fetch(`${BASE_URL}/orders`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ingredients }),
+  }).then(checkResponse);
+};
+
+export const registerRequest = ({ name, email, password }) => {
+    return fetch(`${BASE_URL}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    }).then(checkResponse);
+  };
+
+export const loginRequest = ({ email, password }) => {
+  return fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+};
+
+export const getUser = () => {
+    return fetch(`${BASE_URL}/auth/user`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": localStorage.getItem("accessToken"),
+      },
+    })
+      .then(checkResponse)
+  };
+
+export const forgotPassword = ({ email }) => {
+  return fetch(`${BASE_URL}/auth/rpassword-reset`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+  }).then(checkResponse);
+};
+
+export const resetPassword = ({ token, password }) => {
+  return fetch(`${BASE_URL}/auth/password-reset/reset`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ token, password }),
+  }).then(checkResponse);
+};
+
+export const logout = () => {
+  return fetch(`${BASE_URL}/auth/password-reset/logout`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ token: localStorage.getItem("refreshToken") }),
+  }).then(checkResponse);
 };
 
 
-export const postOrder = ({ingredients}) => {
-    return fetch(`${BASE_URL}/orders`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ingredients})
-    })
-        .then(checkResponse)
-}
 
+export const refreshToken = () => {
+  return fetch(`${BASE_URL}/auth/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({
+      token: localStorage.getItem("refreshToken"),
+    }),
+  })
+    .then(checkResponse)
+    .then((refreshData) => {
+      if (!refreshData.success) {
+        return Promise.reject(refreshData);
+      }
+      localStorage.setItem("refreshToken", refreshData.refreshToken);
+      localStorage.setItem("accessToken", refreshData.accessToken);
+      return refreshData;
+    });
+};
+
+export const fetchWithRefresh = async (url, options) => {
+  try {
+    const res = await fetch(url, options);
+    return await checkResponse(res);
+  } catch (err) {
+    if (err.message === "jwt expired") {
+      const refreshData = await refreshToken(); //обновляем токен
+      options.headers.authorization = refreshData.accessToken;
+      const res = await fetch(url, options); //повторяем запрос
+      return await checkResponse(res);
+    } else {
+      return Promise.reject(err);
+    }
+  }
+};
